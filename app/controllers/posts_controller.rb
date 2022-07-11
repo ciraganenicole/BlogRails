7c9ff4245e35a32823e595af45524a0d5caba11e
ciraganenicole <ciraganenicole@gmail.com>
2022-07-11 22:44:50 +0200

    Add controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,36 +1,5 @@
 class PostsController < ApplicationController
-  def index
-    @user = @user = User.includes(posts: %i[comments likes]).find_by(id: params[:user_id])
-    @post = @user.posts
-  end
+  def index; end
 
-  def show
-    @user = User.find(params[:user_id])
-    @post = @user.posts.find(params[:id])
-    @comment = @post.comments
-  end
-
-  def new
-    @user = User.find(params[:user_id])
-    @post = @user.posts.new
-  end
-
-  def create
-    @user = User.find(params[:user_id])
-    @post = @user.posts.create(post_params)
-    @post.user = @user
-
-    if @post.user
-      redirect_to user_posts_path(@user, @post), notice: 'Post was successfully created!'
-    else
-      render :new
-      flash[:alert] = 'Failed to create a post!'
-    end
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :text)
-  end
+  def show; end
 end
