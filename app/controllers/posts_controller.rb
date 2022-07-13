34a5af5c6f2473ab5abf31d270acfc5709c68aba
ciraganenicole <ciraganenicole@gmail.com>
2022-07-13 16:59:50 +0200

    Add form views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,30 @@ class PostsController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:user_id])
+    # @user = User.find(params[:user_id])
+    @user = current_user
     @post = @user.posts.find(params[:id])
     @comment = @post.comments
+  end
+
+  def new
+    # @user = User.find(params[:user_id])
+    @user = current_user
+    @post = @user.posts.new
+  end
+
+  def create
+    # @user = User.find(params[:user_id])
+    @user = current_user
+    @post = @user.posts.create(post_params)
+
+    @post.user = @user
+    redirect_to user_posts_path(@user)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,6 @@ class User < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: 'user_id', dependent: :destroy
   has_many :likes, class_name: 'Like', foreign_key: 'user_id', dependent: :destroy
 
-  validates :name, presence: true
-  validates :postsCounter, presence: true, numericality: { greater_than_or_equal_to: 0 }
-
   def recent_post
     posts.last(3)
   end
