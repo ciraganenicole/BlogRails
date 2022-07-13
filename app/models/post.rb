b6e32d196330944ff56e1833d0762f21b3216f85
ciraganenicole <ciraganenicole@gmail.com>
2022-07-13 14:34:50 +0200

    Make required changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,6 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id', dependent: :destroy
   has_many :likes, class_name: 'Like', foreign_key: 'post_id', dependent: :destroy
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-  after_save :update_counter
-
-  def update_counter(user_id)
-    count_posts = Post.where(user_id:).count
-    User.find_by(id: user_id).update(postsCounter: count_posts)
-  end
 
   def recent_comments()
     comments.last(5)
