fa8d1159dbdecd0f662037c304809da80469cac8
ciraganenicole <ciraganenicole@gmail.com>
2022-07-22 22:22:57 +0200

    Fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,15 +7,15 @@ class Ability
     can :read, Post
     can :read, User
     can :read, Comment
-    if user.present?
-      if user.role == 'admin'
-        can :manage, :all
-      else
-        can :destroy, Comment, user_id: user.id
-        can :destroy, Post, user_id: user.id
-        can :manage, Post, user_id: user.id
-        can :create, Comment
-      end
+    return unless user.present?
+
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :destroy, Comment, user_id: user.id
+      can :destroy, Post, user_id: user.id
+      can :manage, Post, user_id: user.id
+      can :create, Comment
     end
 
     # The first argument to `can` is the action you are giving the user
