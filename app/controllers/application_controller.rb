3e5d370740352de2cd5ffd7f8eb9a702d8a6ee48
zieeco <zieecosam@gmail.com>
2022-07-23 20:16:10 +0100

    Update the app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
-
- 
+respond_to :json, :html
+  protect_from_forgery unless: -> { request.format.json? }
+  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_back fallback_location: root_path, notice: exception.message }
+      format.js { head :forbidden, content_type: 'text/html' }
+    end
+  end
+  #==
+  protected
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
 end
